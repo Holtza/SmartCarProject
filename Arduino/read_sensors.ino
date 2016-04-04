a854961d7b77d7fb11b9b7d99a123b146916dfe3
Holtza <martina.freiholtz@gmail.com>
2016-04-04 10:12:10 +0200

    All the sensors are being read in the same sketch, helper function created to get rid of redundant code

--- Arduino/read_sensors.ino
+++ Arduino/read_sensors.ino
@@ -21,58 +21,60 @@ void setup() {
 
 }
 
-//Read values for ultrasonic sensors 1 and 2
-int usread1 = 0;
-int usread2 = 0;
+
 
 void loop() {
 
- Serial.print("Sensor 1: ");
-//Instruct sensors to read echoes:
-Wire.beginTransmission(us1); //0x70
+  Serial.print("US 1: ");
+  int sonar1 = ReadSonarSensor(us1);
+  Serial.println(sonar1);
 
-Wire.write(byte(0x00)); //sets register pointer to the command register
-Wire.write(byte(0x51)); //command sensor to measure in cm
-Wire.endTransmission(); //stop transmitting
+  Serial.print("US 2: ");
+  int sonar2 = ReadSonarSensor(us2);
+  Serial.println(sonar2);
 
-delay(70); //Wait for readings to happen
+  Serial.print("IR 1: ");
+  int inred1 = analogRead(IR1);
+  Serial.println(inred1);
 
-Wire.beginTransmission(us1); //transmit to device
-Wire.write(byte(0x02)); //sets register pointer to echo #1 register (0x02)
-Wire.endTransmission(); //stop transmitting
+  Serial.print("IR 2: ");
+  int inred2 = analogRead(IR2);
+  Serial.println(inred2);
 
-Wire.requestFrom(us1, 2); //request two bytes from us1
+  Serial.print("IR 3: ");
+  int inred3 = analogRead(IR3);
+  Serial.println(inred3);
 
-//receiver reading from sensor:
-if (2 <= Wire.available()) { //if 2 bytes were received
-  usread1 = Wire.read(); //receive high byte (overwrites previous reading
-  usread1 = usread1 << 8; //shift high byte as lower 8 bits
-  usread1 |= Wire.read(); //receive low byte as lower 8 bits
-  Serial.println(usread1);
 }
 
-Serial.print("Sensor 2: ");
-//Instruct sensors to read echoes:
-Wire.beginTransmission(us2); //0x72
 
-Wire.write(byte(0x00)); //sets register pointer to the command register
-Wire.write(byte(0x51)); //command sensor to measure in cm
-Wire.endTransmission(); //stop transmitting
 
-delay(70); //Wait for readings to happen
+int ReadSonarSensor(int address){
+  
+  int readValue = 0;
+  
+  Wire.beginTransmission(address);
+  Wire.write(byte(0x00)); //sets register pointer to the command register
+  Wire.write(byte(0x51)); //command sensor to measure in cm
+  Wire.endTransmission(); //stop transmitting
 
-Wire.beginTransmission(us2); //transmit to device
-Wire.write(byte(0x02)); //sets register pointer to echo #1 register (0x02)
-Wire.endTransmission(); //stop transmitting
+  delay(70); //Wait for readings to happen
 
-Wire.requestFrom(us2, 2); //request two bytes from us2
+  Wire.beginTransmission(address); //transmit to device
+  Wire.write(byte(0x02)); //sets register pointer to echo #1 register (0x02)
+  Wire.endTransmission(); //stop transmitting
 
-//receiver reading from sensor:
-if (2 <= Wire.available()) { //if 2 bytes were received
-  usread2 = Wire.read(); //receive high byte (overwrites previous reading
-  usread2 = usread2 << 8; //shift high byte as lower 8 bits
-  usread2 |= Wire.read(); //receive low byte as lower 8 bits
-  Serial.println(usread2);
+  Wire.requestFrom(address, 2); //request two bytes from us1
+
+  //receiver reading from sensor:
+  if (2 <= Wire.available()) { //if 2 bytes were received
+    readValue = Wire.read(); //receive high byte (overwrites previous reading
+    readValue = readValue << 8; //shift high byte as lower 8 bits
+    readValue |= Wire.read(); //receive low byte as lower 8 bits
+  }
+  
+  return readValue;
+  
 }
-//delay(250); //delay for reading output
-}
+  
+ 
