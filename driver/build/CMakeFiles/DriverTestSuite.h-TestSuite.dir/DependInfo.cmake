9e73f96e29fdd4bb69cc6169c0fb62e8a394378f
Tobias Lindell <tobias.lindell@hotmail.com>
2016-04-07 13:17:20 +0200

    compiled driver file necessary for simulation

---
+++ driver/build/CMakeFiles/DriverTestSuite.h-TestSuite.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tobias/Downloads/OpenDaVINCI-3.0.0/automotive/miniature/driver/build/DriverTestSuite.h-TestSuite.cpp" "/home/tobias/Downloads/OpenDaVINCI-3.0.0/automotive/miniature/driver/build/CMakeFiles/DriverTestSuite.h-TestSuite.dir/DriverTestSuite.h-TestSuite.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/tobias/Downloads/OpenDaVINCI-3.0.0/automotive/miniature/driver/build/CMakeFiles/driverlib-static.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../../../cxxtest"
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ driver/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tobias/Downloads/OpenDaVINCI-3.0.0/automotive/miniature/driver
+# Build directory: /home/tobias/Downloads/OpenDaVINCI-3.0.0/automotive/miniature/driver/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(DriverTestSuite.h-TestSuite "/home/tobias/Downloads/OpenDaVINCI-3.0.0/automotive/miniature/driver/build/DriverTestSuite.h-TestSuite")
+SET_TESTS_PROPERTIES(DriverTestSuite.h-TestSuite PROPERTIES  TIMEOUT "3000")
