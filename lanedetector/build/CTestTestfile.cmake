c514b61b1df277878925531db7644ce62c19d3cd
Tobias Lindell <tobias.lindell@hotmail.com>
2016-04-08 14:59:59 +0200

    deleted wrong detector

--- lanedetector/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tobias/Downloads/OpenDaVINCI-3.0.0/automotive/miniature/lanedetector
-# Build directory: /home/tobias/Downloads/OpenDaVINCI-3.0.0/automotive/miniature/lanedetector/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(LaneDetectorTestSuite.h-TestSuite "/home/tobias/Downloads/OpenDaVINCI-3.0.0/automotive/miniature/lanedetector/build/LaneDetectorTestSuite.h-TestSuite")
-SET_TESTS_PROPERTIES(LaneDetectorTestSuite.h-TestSuite PROPERTIES  TIMEOUT "3000")
