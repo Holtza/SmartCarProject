95d3384a194fae51e85ca08cbd428ea72ecfd7ee
Tobias Lindell <tobias.lindell@hotmail.com>
2016-04-29 13:10:09 +0200

    MAJOR REFACTOR: repository should now give a runnable ODV. No more copy pasting cpp files.

---
+++ OpenDaVINCI-3.0.0/README.md
@@ -0,0 +1,216 @@
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="https://github.com/se-research/OpenDaVINCI/blob/master/odlogo/Logo.png" alt="OpenDaVINCI Logo" width=140 height=140>
+# OpenDaVINCI
+
+OpenDaVINCI is a compact middleware OpenDaVINCI written entirely in standard C++.
+It runs on a variety of POSIX-compatible OS. And Windows.
+
+You will find its full documentation and getting started guides here: http://opendavinci.readthedocs.org/en/latest/
+
+You will find different projects that are using OpenDaVINCI here: http://opendavinci.cse.chalmers.se/www/index.html
+
+<a href="https://twitter.com/OpenDaVINCI" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @OpenDaVINCI on Twitter</a>
+
+**Overview build status:**
+
+*Documentation:* [![Documentation Status](https://readthedocs.org/projects/opendavinci/badge/?version=latest)](http://opendavinci.readthedocs.org/en/latest/)
+
+*Lastest source code deployment:* https://github.com/se-research/OpenDaVINCI/releases/latest
+
+*Latest Docker deployment:* https://hub.docker.com/r/seresearch/opendavinci-ubuntu-amd64/tags/
+
+*Latest Docker simulation deployment:* https://hub.docker.com/r/seresearch/odsimulation/tags/
+
+Architecture  | DragonFlyBSD 4.4 | FreeBSD 10.2 | NetBSD 7.0 | OpenBSD 5.8 | Mac OS X
+------------- | ---------------- | ------------ | ------------ | ----------- | --------
+32bit         | - | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-FreeBSD10-32/badge/icon)](https://github.com/se-research/OpenDaVINCI) | - | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-OpenBSD58-32/badge/icon)](https://github.com/se-research/OpenDaVINCI) | -
+64bit         | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-DragonFlyBSD-4.4/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-FreeBSD10/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-NetBSD7.0/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-OpenBSD58/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-MacOSX/badge/icon)](https://github.com/se-research/OpenDaVINCI)
+
+Architecture  | ArchLinux | CentOS 7 | Debian 8.2 | Elementary Freya | Ubuntu 14.04
+------------- | --------- | -------- | ---------- | ---------------- | ------------
+32bit         | -         | -        | -          | -                | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-Ubuntu14.04-32/badge/icon)](https://github.com/se-research/OpenDaVINCI)
+64bit         | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-ArchLinux/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-CentOS7/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-Debian-8.2/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-ElementaryFreya/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-Ubuntu14.04/badge/icon)](https://github.com/se-research/OpenDaVINCI)
+
+Architecture  | Fedora 22 | openSuSE 13.2 | Scientific Linux 7 | Ubuntu 15.04 | Ubuntu 15.10
+------------- | --------- | ------------- | ------------------ | ------------ | ------------
+32bit         | -         | -             | -                  | -            | -
+64bit         | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-Fedora22/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-openSuSE13.2/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-ScientificLinux7/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-Ubuntu15.04/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-Ubuntu15.10/badge/icon)](https://github.com/se-research/OpenDaVINCI)
+
+Architecture  | Windows 7 | Windows 8.1 | Windows 10
+------------- | --------- | ----------- | ----------
+32bit         | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-Windows7/badge/icon)](https://github.com/se-research/OpenDaVINCI) | - | -
+64bit         | -         | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-Windows8.1/badge/icon)](https://github.com/se-research/OpenDaVINCI) | [![Build Status](http://opendavinci.cse.chalmers.se:8888/job/OpenDaVINCI-Windows10/badge/icon)](https://github.com/se-research/OpenDaVINCI)
+
+
+## Using OpenDaVINCI in your Projects
+
+To explore the features of OpenDaVINCI and to use it in your own projects, we provide pre-compiled
+packages in .deb and .rpm format for x86, x86_64, and armfh.
+
+### Compiling OpenDaVINCI from sources
+
+You will find information about how to compile OpenDaVINCI from sources here: http://opendavinci.readthedocs.org/en/latest/installation.html
+
+### Compiling OpenDaVINCI with Clang
+
+To compile OpenDaVINCI using Clang, change to your source folder and create a build directory::
+
+    $ cd OpenDaVINCI && mkdir build
+
+Use cmake to create the build scripts for your build folder and specify the Clang toolchain::
+
+    $ cd build && cmake -D CMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_TOOLCHAIN_FILE=../cmake/clang-Toolchain.cmake ..
+
+### Using our Docker images
+
+We also provide pre-built Docker images for x86_64 and armhf. These images will be automatically updated whenever
+we release new features or patches.
+
+* Ubuntu/x86_64: docker pull seresearch/opendavinci-ubuntu-amd64 (https://registry.hub.docker.com/u/seresearch/opendavinci-ubuntu-amd64/)
+
+* Ubuntu/armhf: docker pull seresearch/opendavinci-ubuntu-armhf (https://registry.hub.docker.com/u/seresearch/opendavinci-ubuntu-armhf/)
+
+### Adding OpenDaVINCI to your Ubuntu 14.04 Linux distribution
+
+To explore the features of OpenDaVINCI and to use it in your own projects, you can add our .deb repository. We provide
+packages for x86, x86_64, and armfh.
+
+Add the public key from our repository:
+
+    $ wget -O - -q http://opendavinci.cse.chalmers.se/opendavinci.cse.chalmers.se.gpg.key | sudo apt-key add -
+
+Add our repository itself to your sources.list:
+
+    $ echo "deb http://opendavinci.cse.chalmers.se/deb/ trusty main" | sudo tee -a /etc/apt/sources.list 
+
+Update your package database:
+
+    $ sudo apt-get update
+
+Install OpenDaVINCI:
+
+    $ sudo apt-get install opendavinci-lib opendavinci-odtools opendavinci-odsupercomponent
+
+
+### Adding OpenDaVINCI to your Ubuntu 15.04 Linux distribution
+
+To explore the features of OpenDaVINCI and to use it in your own projects, you can add our .deb repository. We provide
+packages for x86, x86_64, and armfh.
+
+Add the public key from our repository:
+
+    $ wget -O - -q http://opendavinci.cse.chalmers.se/opendavinci.cse.chalmers.se.gpg.key | sudo apt-key add -
+
+Add our repository itself to your sources.list:
+
+    $ sudo echo "deb http://opendavinci.cse.chalmers.se/deb/ vivid main" >> /etc/apt/sources.list
+
+Update your package database:
+
+    $ sudo apt-get update
+
+Install OpenDaVINCI:
+
+    $ sudo apt-get install opendavinci-lib opendavinci-odtools opendavinci-odsupercomponent
+
+
+### Adding OpenDaVINCI to your Ubuntu 15.10 Linux distribution
+
+To explore the features of OpenDaVINCI and to use it in your own projects, you can add our .deb repository. We provide
+packages for x86, x86_64, and armfh.
+
+Add the public key from our repository:
+
+    $ wget -O - -q http://opendavinci.cse.chalmers.se/opendavinci.cse.chalmers.se.gpg.key | sudo apt-key add -
+
+Add our repository itself to your sources.list:
+
+    $ sudo echo "deb http://opendavinci.cse.chalmers.se/ubuntu-wily/ wily main" >> /etc/apt/sources.list
+
+Update your package database:
+
+    $ sudo apt-get update
+
+Install OpenDaVINCI:
+
+    $ sudo apt-get install opendavinci-lib opendavinci-odtools opendavinci-odsupercomponent
+
+
+### Adding OpenDaVINCI to your Debian 8 Linux distribution
+
+Add the public key from our repository:
+
+    $ wget -O - -q http://opendavinci.cse.chalmers.se/opendavinci.cse.chalmers.se.gpg.key | sudo apt-key add -
+
+Add our repository itself to your sources.list:
+
+    $ sudo echo "deb http://opendavinci.cse.chalmers.se/deb/ jessie main" >> /etc/apt/sources.list
+
+Update your package database:
+
+    $ sudo apt-get update
+
+Install OpenDaVINCI:
+
+    $ sudo apt-get install opendavinci-lib opendavinci-odtools opendavinci-odsupercomponent
+
+
+### Adding OpenDaVINCI to your CentOS 7 Linux distribution
+
+Add our repository to your sources.list:
+
+    $ cd /etc/yum.repos.d && sudo wget http://opendavinci.cse.chalmers.se/OpenDaVINCI-x86_64.repo
+
+Update your package database:
+
+    $ sudo yum -y update
+
+Install OpenDaVINCI:
+
+    $ sudo yum install opendavinci-lib opendavinci-odtools opendavinci-odsupercomponent
+   
+
+### Adding OpenDaVINCI to your Fedora 21 Linux distribution
+
+Add our repository to your sources.list:
+
+    $ cd /etc/yum.repos.d && sudo wget http://opendavinci.cse.chalmers.se/OpenDaVINCI-x86_64.repo
+
+Update your package database:
+
+    $ sudo yum -y update
+
+Install OpenDaVINCI:
+
+    $ sudo yum install opendavinci-lib opendavinci-odtools opendavinci-odsupercomponent
+   
+
+### Adding OpenDaVINCI to your Fedora 22 Linux distribution
+
+Add our repository to your sources.list:
+
+    $ cd /etc/yum.repos.d && sudo wget http://opendavinci.cse.chalmers.se/OpenDaVINCI-x86_64.repo
+
+Update your package database:
+
+    $ sudo dnf -y upgrade
+
+Install OpenDaVINCI:
+
+    $ sudo dnf install opendavinci-lib opendavinci-odtools opendavinci-odsupercomponent
+
+
+### Adding OpenDaVINCI to your openSuSE 13 Linux distribution
+
+Add our repository to your sources.list:
+
+    $ cd /etc/zypp/repos.d && sudo wget http://opendavinci.cse.chalmers.se/OpenDaVINCI-x86_64.repo
+
+Update your package database:
+
+    $ sudo zypper --no-gpg-checks refresh
+
+Install OpenDaVINCI:
+
+    $ sudo zypper install opendavinci-lib opendavinci-odtools opendavinci-odsupercomponent
+
+![Screenshot](https://github.com/se-research/OpenDaVINCI/blob/master/docs/Screenshot.png)
---
+++ OpenDaVINCI-3.0.0/odcockpit/src/plugins/livefeed/LiveFeedWidget.cpp
@@ -0,0 +1,286 @@
+/**
+ * cockpit - Visualization environment
+ * Copyright (C) 2012 - 2016 Christian Berger
+ * Copyright (C) 2008 - 2011 (as monitor component) Christian Berger, Bernhard Rumpe
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <Qt/qgridlayout.h>
+#include <Qt/qtreewidget.h>
+#include <qstring.h>
+#include <qstringlist.h>
+
+#include <cstring>
+#include <vector>
+
+#include "opendavinci/odcore/opendavinci.h"
+#include "opendavinci/odcore/base/Visitable.h"
+#include "opendavinci/odcore/data/Container.h"
+#include "opendavinci/odcore/data/TimeStamp.h"
+#include "automotivedata/generated/automotive/ForceControl.h"
+#include "automotivedata/generated/automotive/GenericCANMessage.h"
+#include "automotivedata/generated/automotive/VehicleData.h"
+#include "automotivedata/generated/automotive/miniature/UserButtonData.h"
+#include "automotivedata/generated/automotive/vehicle/WheelSpeed.h"
+#include "opendavinci/generated/odcore/data/Configuration.h"
+#include "opendavinci/generated/odcore/data/SharedData.h"
+#include "opendavinci/generated/odcore/data/dmcp/DiscoverMessage.h"
+#include "opendavinci/generated/odcore/data/dmcp/ModuleDescriptor.h"
+#include "opendavinci/generated/odcore/data/dmcp/ModuleExitCodeMessage.h"
+#include "opendavinci/generated/odcore/data/dmcp/ModuleStateMessage.h"
+#include "opendavinci/generated/odcore/data/dmcp/ModuleStatistics.h"
+#include "opendavinci/generated/odcore/data/dmcp/RuntimeStatistic.h"
+#include "opendavinci/generated/odcore/data/image/SharedImage.h"
+#include "opendavinci/generated/odcore/data/player/PlayerCommand.h"
+#include "opendavinci/generated/odcore/data/recorder/RecorderCommand.h"
+#include "plugins/livefeed/LiveFeedWidget.h"
+#include "plugins/livefeed/MessageToTupleVisitor.h"
+
+namespace cockpit { namespace plugins { class PlugIn; } }
+
+namespace cockpit {
+
+    namespace plugins {
+
+        namespace livefeed {
+
+            using namespace std;
+            using namespace odcore::base;
+            using namespace odcore::data;
+
+            LiveFeedWidget::LiveFeedWidget(const PlugIn &/*plugIn*/, QWidget *prnt) :
+                QWidget(prnt),
+                m_dataView(),
+                m_dataToType() {
+                // Set size.
+                setMinimumSize(640, 480);
+
+                // Layout manager.
+                QGridLayout* mainBox = new QGridLayout(this);
+
+                //ListView and header construction
+                m_dataView = unique_ptr<QTreeWidget>(new QTreeWidget(this));
+                m_dataView->setColumnCount(2);
+                QStringList headerLabel;
+                headerLabel << tr("Message") << tr("Value");
+                m_dataView->setColumnWidth(0, 200);
+                m_dataView->setColumnWidth(1, 200);
+                m_dataView->setHeaderLabels(headerLabel);
+
+                //add to Layout
+                mainBox->addWidget(m_dataView.get(), 0, 0);
+
+                // Set layout manager.
+                setLayout(mainBox);
+            }
+
+            LiveFeedWidget::~LiveFeedWidget() {}
+
+            void LiveFeedWidget::nextContainer(Container &container) {
+                transformContainerToTree(container);
+            }
+
+            void LiveFeedWidget::addMessageToTree(const string &messageName, odcore::data::Container &container, odcore::base::Visitable &v) {
+                //create new Header if needed
+                if (m_dataToType.find(messageName) == m_dataToType.end()) {
+                    QTreeWidgetItem *newHeader = new QTreeWidgetItem(m_dataView.get());
+                    newHeader->setText(0, messageName.c_str());
+                    m_dataToType[messageName] = newHeader;
+                }
+
+                vector<pair<string, string> > entries;
+                entries.push_back(make_pair("Sent", container.getSentTimeStamp().getYYYYMMDD_HHMMSSms()));
+                entries.push_back(make_pair("Received", container.getReceivedTimeStamp().getYYYYMMDD_HHMMSSms()));
+
+                // Map attributes from message to the entries.
+                MessageToTupleVisitor mttv(entries);
+                v.accept(mttv);
+
+                QTreeWidgetItem *entry = m_dataToType[messageName];
+                if (static_cast<uint32_t>(entry->childCount()) != entries.size()) {
+                    entry->takeChildren();
+
+                    for (uint32_t i = 0; i < entries.size(); i++) {
+                        QTreeWidgetItem *sent = new QTreeWidgetItem();
+                        entry->insertChild(i, sent);
+                    }
+                }
+
+                // Map tuples of <string, string> to the tree.
+                for (uint32_t i = 0; i < entries.size(); i++) {
+                    QTreeWidgetItem *child = entry->child(i);
+                    child->setText(0, entries.at(i).first.c_str());
+                    child->setText(1, entries.at(i).second.c_str());
+                }
+            }
+
+            void LiveFeedWidget::transformContainerToTree(Container &container) {
+/*
+                if (container.getDataType() == coredata::dmcp::ModuleStatistics::ID()) {
+                    coredata::dmcp::ModuleStatistics tmp = container.getData<coredata::dmcp::ModuleStatistics>();
+                    if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                        addMessageToTree(tmp.LongName(), container, tmp);
+                    }
+                    return;
+                }
+*/
+
+                if (container.getDataType() == automotive::VehicleData::ID()) {
+                    automotive::VehicleData tmp = container.getData<automotive::VehicleData>();
+                    if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                        addMessageToTree(tmp.LongName(), container, tmp);
+                    }
+                    return;
+                }
+
+/*
+                switch (container.getDataType()) {
+                    case Container::CONFIGURATION:
+                    {
+                        odcore::data::Configuration tmp = container.getData<odcore::data::Configuration>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::FORCECONTROL:
+                    {
+                        automotive::ForceControl tmp = container.getData<automotive::ForceControl>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::DMCP_DISCOVER:
+                    {
+                        odcore::data::dmcp::DiscoverMessage tmp = container.getData<odcore::data::dmcp::DiscoverMessage>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::DMCP_CONFIGURATION_REQUEST:
+                    {
+                        odcore::data::dmcp::ModuleDescriptor tmp = container.getData<odcore::data::dmcp::ModuleDescriptor>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::DMCP_MODULESTATEMESSAGE:
+                    {
+                        odcore::data::dmcp::ModuleStateMessage tmp = container.getData<odcore::data::dmcp::ModuleStateMessage>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::DMCP_MODULEEXITCODEMESSAGE:
+                    {
+                        odcore::data::dmcp::ModuleExitCodeMessage tmp = container.getData<odcore::data::dmcp::ModuleExitCodeMessage>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::MODULESTATISTICS:
+                    {
+                        odcore::data::dmcp::ModuleStatistics tmp = container.getData<odcore::data::dmcp::ModuleStatistics>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::VEHICLEDATA:
+                    {
+                        automotive::VehicleData tmp = container.getData<automotive::VehicleData>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::PLAYER_COMMAND:
+                    {
+                        odcore::data::player::PlayerCommand tmp = container.getData<odcore::data::player::PlayerCommand>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::RECORDER_COMMAND:
+                    {
+                        odcore::data::recorder::RecorderCommand tmp = container.getData<odcore::data::recorder::RecorderCommand>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::RUNTIMESTATISTIC:
+                    {
+                        odcore::data::dmcp::RuntimeStatistic tmp = container.getData<odcore::data::dmcp::RuntimeStatistic>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::SHARED_DATA:
+                    {
+                        odcore::data::SharedData tmp = container.getData<odcore::data::SharedData>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::SHARED_IMAGE:
+                    {
+                        odcore::data::image::SharedImage tmp = container.getData<odcore::data::image::SharedImage>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::USER_BUTTON:
+                    {
+                        automotive::miniature::UserButtonData tmp = container.getData<automotive::miniature::UserButtonData>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::GENERIC_CAN_MESSAGE:
+                    {
+                        automotive::GenericCANMessage tmp = container.getData<automotive::GenericCANMessage>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    case Container::WHEELSPEED:
+                    {
+                        automotive::vehicle::WheelSpeed tmp = container.getData<automotive::vehicle::WheelSpeed>();
+                        if (dynamic_cast<Visitable*>(&tmp) != NULL) {
+                            addMessageToTree(tmp.LongName(), container, tmp);
+                        }
+                        break;
+                    }
+                    default:
+                    break;
+                }
+*/
+            }
+        }
+    }
+}
